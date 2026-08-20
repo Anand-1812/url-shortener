eb87e7e92731d66d330e935ddfbb7f83fedba5d9
Anand-1812 <anand.work2005@gmail.com>
2026-08-20 11:24:58 +0530

    Add: frontend for url shortener

---
+++ frontend/app/page.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen w-full bg-neutral-950 text-white flex flex-col items-center p-4 overflow hidden">
+      <h1 className="text-4xl font-bold text-center mt-10">
+        URL Shortener
+      </h1>
+
+      <div className="w-full max-w-md mt-16 p-6 rounded-xl shadow-lg border border-gray-200">
+        {/* Form goes here */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
