6013719fe1504e003d79c5f1ab7394067cb27c55
Anand-1812 <anand.work2005@gmail.com>
2026-08-31 14:05:03 +0530

    Add: backend connection with pg

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I love %s!", r.URL.Path[1:])
+type Server struct {
+	db *pgxpool.Pool
+}
+
+// method for the handler
+func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is from %s", r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	_ = godotenv.Load()
+
+	dbConnString := os.Getenv("NEON_DB_STRING")
+	if dbConnString == "" {
+		log.Fatal("DB env is required")
+	}
+
+	pool, err := pgxpool.New(context.Background(), dbConnString)
+	if err != nil {
+		log.Fatal("Error connecting to db")
+	}
+	defer pool.Close()
+
+	fmt.Println("Connection established")
+
+	server := &Server{db: pool}
+
+	http.HandleFunc("/", server.handler)
+	log.Println("server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
