bfba0764417d07669406bebd549802906d172de7
Anand-1812 <anand.work2005@gmail.com>
2026-09-03 09:19:00 +0530

    feat: add enableCORS

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -16,6 +16,21 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	_ = godotenv.Load()
 	ctx := context.Background()
@@ -59,5 +74,5 @@ func main() {
 	}
 
 	log.Println("server running on :8080")
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	log.Fatal(http.ListenAndServe(":"+port, enableCors(mux)))
 }
